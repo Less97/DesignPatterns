46f6745f0074c3f59eeaf576b713f00f02163d35
Less97 <Alessandro83Lignano@gmail.com>
2015-03-10 16:47:08 +0000

    [Enhancement] Proxy Pattern

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -10,6 +10,21 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Proxy Pattern:");
+            Proxy myProxy = new Proxy();
+            //should not grant the access
+            Console.WriteLine("Request Without access...");
+            Console.WriteLine(myProxy.Request());
+            //ask for authentication...Failed
+            Console.WriteLine("Authentication answer: " + myProxy.Authenticate("wrongUser", "wrongPassword"));
+            //Request Failed again
+            Console.WriteLine("Request after failing access...");
+            Console.WriteLine(myProxy.Request());
+            //Request Granted...
+            Console.WriteLine("Authentication answer: " + myProxy.Authenticate("user", "password"));
+            Console.WriteLine("Request after granted access...");
+            Console.WriteLine(myProxy.Request());
+            Console.ReadLine();
         }
     }
 }
---
+++ Proxy/Proxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class Proxy : IServer
+    {
+        private Server hiddenServer = new Server();
+        private Boolean isAuthenticated = false;
+        public String Authenticate(String username, String password)
+        {
+            if (username == "user" && password == "password")
+            {
+                isAuthenticated = true;
+                return "Authorization Granted.";
+            }
+            else
+            {
+                return "Authorization Denied.";
+            }
+        }
+
+        public String Request()
+        {
+            if (isAuthenticated)
+            {
+                return hiddenServer.Request();
+            }
+            else
+            {
+                return "Operation not Authorized. Authenticate First";
+            }
+        }
+
+       
+    }
+}
