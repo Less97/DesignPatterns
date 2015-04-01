3da43a762e49b5707b466dfba5f66bfe72776f66
Less97 <Alessandro83Lignano@gmail.com>
2015-04-01 11:05:12 +0100

    [Enhancement] Introduced Adapter pattern + Composition working

---
+++ Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOperator op = new OperationAdapter();
+            op.Operation().Operation();
+            Console.ReadLine();
+
+        }
+    }
+}
